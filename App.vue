c617dd1bf4bc41fb47b47e3550df3d7a2c30f383
Mr.Mao <951416545@qq.com>
2024-07-24 21:17:00 +0800

    chore: update docs

--- App.vue
+++ App.vue
@@ -1,24 +1,24 @@
 <script lang="ts" setup>
-import { useFrontmatter } from 'valaxy'
-import { nextTick, onMounted, ref } from 'vue'
-import { useRouter } from 'vue-router'
+// import { useFrontmatter } from 'valaxy'
+// import { nextTick, onMounted, ref } from 'vue'
+// import { useRouter } from 'vue-router'
 
-const frontmatter = useFrontmatter()
-const router = useRouter()
-const meting = ref()
+// const frontmatter = useFrontmatter()
+// const router = useRouter()
+// const meting = ref()
 
-router.afterEach(() => {
-  if (frontmatter.value.tags?.includes('music'))
-    return
-  setTimeout(() => meting.value?.aplayer?.play?.(), 500)
-})
+// router.afterEach(() => {
+//   if (frontmatter.value.tags?.includes('music'))
+//     return
+//   setTimeout(() => meting.value?.aplayer?.play?.(), 500)
+// })
 
-onMounted(async () => {
-  await nextTick()
-  meting.value = document.querySelector('meting-js')
-  if (frontmatter.value.tags?.includes('music'))
-    return
-  meting.value?.setAttribute('theme', 'var(--hy-c-primary)')
-  meting.value?.setAttribute('autoplay', 'true')
-})
+// onMounted(async () => {
+//   await nextTick()
+//   meting.value = document.querySelector('meting-js')
+//   if (frontmatter.value.tags?.includes('music'))
+//     return
+//   meting.value?.setAttribute('theme', 'var(--hy-c-primary)')
+//   meting.value?.setAttribute('autoplay', 'true')
+// })
 </script>
